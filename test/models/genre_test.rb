c6d555a977df40019db92da86ef583b5403c8cb9
Arek Krawczyk <arkadiusz.krawczyk@nsn.com>
2015-01-13 17:30:31 +0100

    Added genre test.

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@genre = genres(:one)
+	end
+
+	test "should be valid " do
+		assert @genre.valid?
+	end
+
+	test "should be invalid without name" do
+		@genre.name = nil
+		assert_not @genre.valid?
+	end
+
 end
